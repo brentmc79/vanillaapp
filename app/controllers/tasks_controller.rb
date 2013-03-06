45fa12808633461d41347734c6d28a1479e0b9e5
Brent Collier <bcollier@twitter.com>
2013-03-05 17:04:28 -0800

    Enable task deletion

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -80,7 +80,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     respond_to do |format|
-      format.html { redirect_to tasks_url }
+      format.html { redirect_to @list }
       format.json { head :no_content }
     end
   end
